7791cb013d86374f1900860abc127146cda4a669
Yassir Wahid <yassir7t@gmail.com>
2024-12-06 09:47:52 +0100

    init

---
+++ src/main/java/fr/ubo/tp_djf/BackendTpDjfApplication.java
@@ -0,0 +1,13 @@
+package fr.ubo.tp_djf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTpDjfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTpDjfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ubo/tp_djf/BackendTpDjfApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubo.tp_djf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTpDjfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
