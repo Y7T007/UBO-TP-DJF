264095595fb2d2dcfaff7b5b7862678f82688f2d
Nada El baghdadi <nada.elbaghdadi@etu.uae.ac.ma>
2024-12-06 11:14:11 +0100

    controlleurService

---
+++ src/main/java/fr/ubo/tp_djf/controllers/EtudiantController.java
@@ -0,0 +1,42 @@
+package fr.ubo.tp_djf.controllers;
+import fr.ubo.tp_djf.models.Etudiant;
+import fr.ubo.tp_djf.services.EtudiantService;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+public class EtudiantController {
+    private final EtudiantService etudiantService;
+
+
+    public EtudiantController(EtudiantService etudiantService) {
+        this.etudiantService = etudiantService;
+    }
+
+    @GetMapping
+    public List<Etudiant> getAllEtudiants() {
+        return etudiantService.getAllEtudiants();
+    }
+
+    @GetMapping("/{id}")
+    public Etudiant getEtudiantById(@PathVariable String id) {
+        return etudiantService.getEtudiantById(id);
+    }
+
+    @PostMapping
+    public Etudiant createEtudiant(@RequestBody Etudiant etudiant) {
+        return etudiantService.saveEtudiant(etudiant);
+    }
+
+    @PutMapping("/{id}")
+    public Etudiant updateEtudiant(@PathVariable String id, @RequestBody Etudiant etudiant) {
+        etudiant.setNoEtudiantNat(id);
+        return etudiantService.saveEtudiant(etudiant);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEtudiant(@PathVariable String id) {
+        etudiantService.deleteEtudiant(id);
+    }
+}
+
---
+++ src/main/java/fr/ubo/tp_djf/services/EtudiantService.java
@@ -0,0 +1,7 @@
+package fr.ubo.tp_djf.services;
+
+
+public class EtudiantService {
+
+
+}
