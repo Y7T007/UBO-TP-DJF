a312eb9ca6534dd8ac7d7daf5107b3464c0836e5
Yassir Wahid <yassir7t@gmail.com>
2024-12-06 11:09:16 +0100

    Hibernate Postgre Linked

--- src/main/java/fr/ubo/tp_djf/BackendTpDjfApplication.java
+++ src/main/java/fr/ubo/tp_djf/BackendTpDjfApplication.java
@@ -1,5 +1,6 @@
 package fr.ubo.tp_djf;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendTpDjfApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
         SpringApplication.run(BackendTpDjfApplication.class, args);
     }
-
 }
