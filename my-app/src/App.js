7733edee9d0bdd4547f84b5c7a185dd46c284e92
houaELM02 <houda.elmoumouhi@gmail.com>
2024-12-06 11:16:02 +0100

    List Etudiant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,17 +6,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <Router>
-    
-   
-      <Routes>
-        <Route path='/' element={<ListEtudiant />} />
-       
-        </Routes>
-    
+    <Router>
+    <Routes>
+      <Route path="/" element={<ListEtudiant />} />
+    </Routes>
   </Router>
-    </div>
   );
 }
 
--- my-app/src/Components/ListEtudiant.jsx
+++ my-app/src/Components/ListEtudiant.jsx
@@ -3,29 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchEtudiants, selectEtudiants, selectLoading, selectError } from '../redux/ListSlice';
 
 const ListEtudiant = () => {
-  const dispatch = useDispatch();
-  const etudiants = useSelector(selectEtudiants);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchEtudiants()); // Appelle l'API pour récupérer les étudiants
-  }, [dispatch]);
-
-  if (loading) return <p>Chargement des étudiants...</p>;
-  if (error) return <p>Erreur : {error}</p>;
-
+  
   return (
     <div>
       <h2>Liste des étudiants</h2>
-      <ul>
-        {etudiants.map((etudiant) => (
-          <li key={etudiant.noEtudiantNat}>
-            {etudiant.nom} {etudiant.prenom} - {etudiant.email} - Diplômé :{' '}
-            {etudiant.estDiplome === 'Y' ? 'Oui' : 'Non'}
-          </li>
-        ))}
-      </ul>
+     
     </div>
   );
 };
