d50e82897a10cccfa9dc2d55e1bb7f6f4bd07003
Y7T007 <yassir.wahid@etu.uae.ac.ma>
2024-12-15 14:45:56 +0100

    Hot Fix V1.0.0

--- DataBase/SD-SPI/02-CSCI-ADM/csci_adm_installation.sql
+++ DataBase/SD-SPI/02-CSCI-ADM/csci_adm_installation.sql
@@ -5,21 +5,9 @@
 
 -- Cr�ation des �l�ments DDL :  tables, vues, index, ...
 @@01-CREATION/01-DDL/csci_adm_tab.sql
-@@01-CREATION/01-DDL/csci_adm_vw.sql
 @@01-CREATION/01-DDL/csci_adm_pk.sql
-@@01-CREATION/01-DDL/csci_adm_fk.sql
 @@01-CREATION/01-DDL/csci_adm_ck.sql
-@@01-CREATION/01-DDL/csci_adm_ind.sql
-@@01-CREATION/01-DDL/csci_adm_seq.sql
-@@01-CREATION/01-DDL/csci_adm_avt.sql
 
 -- Cr�ation du jeu d'essai
-@@02-JEU-ESSAI/promotion.sql
-@@02-JEU-ESSAI/enseignant.sql
-@@02-JEU-ESSAI/entreprise.sql
-@@02-JEU-ESSAI/employe.sql
-@@02-JEU-ESSAI/candidat.sql
-@@02-JEU-ESSAI/etudiant.sql
-@@02-JEU-ESSAI/diplome.sql
-@@02-JEU-ESSAI/poste_entreprise.sql
+@@02-JEU-ESSAI/script.sql
 
--- DataBase/init.sh
+++ DataBase/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Save the starting directory
+START_DIR=$(pwd)
+
 # Check if the initialization is already complete
 if [ ! -f /tmp/setup_complete ]; then
     echo "Waiting for Oracle DB to start..."
@@ -16,6 +19,43 @@ if [ ! -f /tmp/setup_complete ]; then
     grant all privileges to SYS;
     EXIT;
 EOF
+
+    echo "Running custom SQL scripts from /docker-entrypoint-initdb.d/SD-SPI..."
+
+    # Execute specific scripts in each folder
+    if [ -f /docker-entrypoint-initdb.d/SD-SPI/01-CSCI-GLOB/csci_glob_installation.sql ]; then
+        echo "Executing folder1_installation.sql..."
+
+        # cd into the correct folder before running the script
+        cd /docker-entrypoint-initdb.d/SD-SPI/01-CSCI-GLOB
+        sqlplus DOSI/DOSI@localhost:1521/XE <<EOF
+        @csci_glob_installation.sql
+        COMMIT;
+        EXIT;
+EOF
+    else
+        echo "No folder1_installation.sql found in folder1"
+    fi
+
+    if [ -f /docker-entrypoint-initdb.d/SD-SPI/02-CSCI-ADM/csci_adm_installation.sql ]; then
+        echo "Executing folder2_installation.sql..."
+
+        # cd into the correct folder before running the script
+        cd /docker-entrypoint-initdb.d/SD-SPI/02-CSCI-ADM
+        sqlplus DOSI/DOSI@localhost:1521/XE <<EOF
+        @csci_adm_installation.sql
+        COMMIT;
+        EXIT;
+EOF
+    else
+        echo "No folder2_installation.sql found in folder2"
+    fi
+
+    echo "Initialization and custom scripts executed successfully."
+
+    # Return to the original directory before marking setup as complete
+    cd "$START_DIR"
+
     # Mark setup as complete
     echo "Setup complete!" > /tmp/setup_complete
 else
